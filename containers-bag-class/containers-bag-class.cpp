760010872cdcfd25180db1c9891830899bf455a3
leer89 <leer89@gmail.com>
2017-02-23 17:05:35 -0500

    added post conditions

--- containers-bag-class/containers-bag-class.cpp
+++ containers-bag-class/containers-bag-class.cpp
@@ -1,11 +1,67 @@
-// containers-bag-class.cpp : Defines the entry point for the console application.
-//
+
+// FILE: containers-bag-class
+// Demonstration of container class type bag
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+// PROTOTYPES
+void get_ages(bag& ages);
+/* Postcondition:
+The user has been prompted to type in the ages of family members. 
+These ages have been read and placed in the ages bag, stopping 
+when the bag is full or when the user types a negative number.
+*/
+
+void check_ages(bag& ages);
+/* Postcondition:
+The user has been prompted to type in the ages of family members
+again. Each age is removed from the ages bag when it is typed, stopping
+when the bag is empty.
+*/
 
 int main()
 {
-    return 0;
+	bag ages;
+
+	get_ages(ages);
+	check_ages(ages);
+	cout << "May your family live long and prosper." << endl;
+    return EXIT_SUCCESS;
 }
 
+void get_ages(bag& ages)
+{
+	int user_input;
+
+	cout << "Type the ages in your family." << endl;
+	cout << "Type a negative number when you are done: " << endl;
+
+	cin >> user_input;
+	while (user_input >= 0)
+	{
+		if (ages.size() < ages.CAPACITY)
+			ages.insert(user_input);
+		else
+			cout << "I have run out of room and can't add that age." << endl;
+		cin >> user_input;
+	}
+}
+
+void check_ages(bag& ages)
+{
+	int user_input;
+
+	cout << "Type those ages again. Press return after each age:" << endl;
+	while (ages.size() > 0)
+	{
+		cin >> user_input;
+		if (ages.erase_one(user_input))
+			cout << "Yes, I've found that age and removed it." << endl;
+		else
+			cout << "No, that age does not occur!" << endl;
+	}
+}
